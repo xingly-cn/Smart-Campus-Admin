7f2457c68a2be72a663162c7edf16bf8a446b2ce
肖念昕 <sugar_tech@outlook.com>
2022-03-03 23:16:25 +0800

    仪表盘

--- src/view/single-page/home/home.vue
+++ src/view/single-page/home/home.vue
@@ -11,7 +11,7 @@
     <Row :gutter="20" style="margin-top: 10px;">
       <i-col :md="24" :lg="8" style="margin-bottom: 20px;">
         <Card shadow>
-          <chart-pie style="height: 300px;" :value="pieData" text="用户访问来源"></chart-pie>
+          <chart-pie style="height: 300px;" :value="pieData" text="失物分类统计"></chart-pie>
         </Card>
       </i-col>
       <i-col :md="24" :lg="16" style="margin-bottom: 20px;">
@@ -44,20 +44,14 @@ export default {
   },
   data () {
     return {
-      inforCardData: [
-        { title: '新增用户', icon: 'md-person-add', count: 803, color: '#2d8cf0' },
-        { title: '累计点击', icon: 'md-locate', count: 232, color: '#19be6b' },
-        { title: '新增问答', icon: 'md-help-circle', count: 142, color: '#ff9900' },
-        { title: '分享统计', icon: 'md-share', count: 657, color: '#ed3f14' },
-        { title: '新增互动', icon: 'md-chatbubbles', count: 12, color: '#E46CBB' },
-        { title: '新增页面', icon: 'md-map', count: 14, color: '#9A66E4' }
-      ],
+      count: [67, 332, 243, 234, 23, 43],
+      user: 999,
       pieData: [
-        { value: 335, name: '直接访问' },
-        { value: 310, name: '邮件营销' },
-        { value: 234, name: '联盟广告' },
-        { value: 135, name: '视频广告' },
-        { value: 1548, name: '搜索引擎' }
+        { value: 335, name: '电子产品' },
+        { value: 310, name: '书籍' },
+        { value: 234, name: '日用品' },
+        { value: 135, name: '快递' },
+        { value: 1548, name: '交通工具' }
       ],
       barData: {
         Mon: 13253,
@@ -70,8 +64,21 @@ export default {
       }
     }
   },
+  computed: {
+    inforCardData () {
+      return [
+        { title: '用户统计', icon: 'md-person-add', count: this.user, color: '#2d8cf0' },
+        { title: '物品寻回', icon: 'md-locate', count: 232, color: '#19be6b' },
+        { title: '物品统计', icon: 'md-help-circle', count: 142, color: '#ff9900' },
+        { title: '学校统计', icon: 'md-share', count: 657, color: '#ed3f14' },
+        { title: '消息统计', icon: 'md-chatbubbles', count: 12, color: '#E46CBB' },
+        { title: '反馈统计', icon: 'md-map', count: 14, color: '#9A66E4' }
+      ]
+    }
+  },
   mounted () {
     //
+    console.log(this.inforCardData, 'inforCardData')
   }
 }
 </script>
